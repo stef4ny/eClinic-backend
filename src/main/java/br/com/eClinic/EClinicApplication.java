cef9bca929253a0d6e05680c15bb71b2cecbca15
WendelEfraim <wendel.efrain11@gmail.com>
2024-12-01 04:56:50 -0300

    escutando

--- src/main/java/br/com/eClinic/EClinicApplication.java
+++ src/main/java/br/com/eClinic/EClinicApplication.java
@@ -2,8 +2,10 @@ package br.com.eClinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EClinicApplication {
 
 	public static void main(String[] args) {
