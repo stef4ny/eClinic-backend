90c5adc930dcbe087b9f7a26d53bc649600c936d
WendelEfraim <wendel.efrain11@gmail.com>
2024-12-09 01:59:50 -0300

    concluindo put de paciente e add get dos pacientes por id/all

--- src/main/java/br/com/eClinic/modelo/paciente/PacienteService.java
+++ src/main/java/br/com/eClinic/modelo/paciente/PacienteService.java
@@ -1,6 +1,8 @@
 package br.com.eClinic.modelo.paciente;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,36 @@ import jakarta.transaction.Transactional;
 public class PacienteService {
 
     @Autowired
-   private PacienteRepository repository;
+    private PacienteRepository repository;
 
-   @Transactional
-   public Paciente save(Paciente paciente) {
+    @Transactional
+    public Paciente save(Paciente paciente) {
 
-       paciente.setHabilitado(Boolean.TRUE);
-       return repository.save(paciente);
-   }
+        paciente.setHabilitado(Boolean.TRUE);
+        return repository.save(paciente);
+    }
 
+    @Transactional
+    public void update(Long id, Paciente pacienteAlterado) {
+
+        Paciente paciente = repository.findById(id).get();
+        paciente.setNomeCompleto(pacienteAlterado.getNomeCompleto());
+        paciente.setDataNascimento(pacienteAlterado.getDataNascimento());
+        paciente.setCpf(pacienteAlterado.getCpf());
+        paciente.setSenha(pacienteAlterado.getSenha());
+        paciente.setEnderecoCidade(pacienteAlterado.getEnderecoCidade());
+        paciente.setEnderecoUf(pacienteAlterado.getEnderecoUf());
+        
+        repository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Paciente obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
 }
