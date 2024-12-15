5bac9a7d204429506f0a34ec367e64935431068c
WendelEfraim <wendel.efrain11@gmail.com>
2024-12-15 13:35:41 -0300

    sem login-usuario

--- src/main/java/br/com/eClinic/modelo/usuario/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.eClinic.modelo.usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
-}
