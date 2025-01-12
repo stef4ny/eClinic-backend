01edbe048418a488062e315cb8593031f94baa7a
david <da-vidps@hotmail.com>
2025-01-12 19:47:06 -0300

    tratador de erros

--- src/main/java/br/com/eClinic/modelo/paciente/Paciente.java
+++ src/main/java/br/com/eClinic/modelo/paciente/Paciente.java
@@ -2,13 +2,17 @@ package br.com.eClinic.modelo.paciente;
 
 import java.time.LocalDate;
 
+
+import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.SQLRestriction;
 
 import br.com.eClinic.modelo.acesso.Usuario;
 import br.com.eClinic.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
